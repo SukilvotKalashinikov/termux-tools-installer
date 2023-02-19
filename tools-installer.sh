cc9aa284cf376dc3ee68ebcb8a721ee8a7f80fd2
Sukilovot Kalashinikov <107007680+SukilvotKalashinikov@users.noreply.github.com>
2023-02-19 19:21:50 -0300

    Add files via upload

---
+++ tools-installer.sh
@@ -0,0 +1,38 @@
+# made by sukilovot
+
+install()
+{
+	git $url
+}
+
+echo "starting to update apt..."
+
+apt update -y && apt upgrade -y
+
+echo "starting to update pkg..."
+
+pkg update -y && pkg upgrade -y
+
+echo "installing nmap..."
+
+pkg install nmap
+
+echo "installing sqlmap..."
+
+url="https://github.com/sqlmapproject/sqlmap"
+install
+
+echo "installing camhackers..."
+
+url="https://github.com/AngelSecurityTeam/Cam-Hackers"
+install
+
+echo "installing cam hacker..."
+
+url="https://github.com/KasRoudra/CamHacker"
+install
+
+echo "installing hydra..."
+
+url=https://github.com/vanhauser-thc/thc-hydra
+install
